ca6255ba1b68df03bcc8639533cf5379d36f08b8
Fluiz19 <francisco_luiz19@hotmail.com>
2024-01-21 16:18:09 -0300

    Desafio resolvido

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("hello world");
+let vitorias ;
+let derrotas ;
+let saldoDeVitorias = saldo ( 200, 130);
+let nivel = nivelJogador (saldoDeVitorias);
+
+
+
+function saldo (vitorias, derrotas){
+    return  vitorias - derrotas;
+    
+}
+
+console.log(saldoDeVitorias);
+console.log(nivel);
+
+
+function nivelJogador( saldoDeVitorias){
+     if ( saldoDeVitorias <= 10){
+            return "Ferro";
+     }
+    if (saldoDeVitorias >=11 && saldoDeVitorias <=20){
+            return "Bronze";
+    }
+    if (saldoDeVitorias >=21 && saldoDeVitorias <=50){
+        return "Prata";
+    }
+    if (saldoDeVitorias >=51 && saldoDeVitorias <=80){
+        return "Ouro";
+    }
+    if (saldoDeVitorias >= 81 && saldoDeVitorias <=90){
+        return "Diamante";
+    }
+    if (saldoDeVitorias >= 91 && saldoDeVitorias <= 100){
+        return "Lendário";
+    }
+    if (saldoDeVitorias >101){
+        return "Imortal";
+    }
+ }
+ console.log ("O herói tem um saldo de " + saldoDeVitorias + " e está no nivel " + nivel);
+
+
+
+
